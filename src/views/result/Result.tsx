d31f90c8e01dd8cbef0bcea523582a3825b5da2d
Twedmark <axel.twedmark@hotmail.com>
2023-03-14 09:57:08 +0100

    fetch random card and display it

--- src/views/result/Result.tsx
+++ src/views/result/Result.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import logo from "./../../logo.svg";
 import "./../../App.css";
 
-function App() {
+function App(props: any) {
+  const navigate = useNavigate();
+  const { card } = props;
+
+  function home() {
+    console.log("home");
+    navigate("/");
+  }
+
   return (
     <div className="App">
       Result
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" onClick={home} />
+      <>
+        <img src={card?.image_uris.border_crop} alt={card?.name}></img>
+      </>
     </div>
   );
 }
--- src/views/search/Search.tsx
+++ src/views/search/Search.tsx
@@ -1,15 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import logo from "./../../logo.svg";
 import "./../../App.css";
 
-function App() {
+function Search(props: any) {
+  const { setCard } = props;
+  const navigate = useNavigate();
+
+  async function fetchTest() {
+    const data = await fetch("https://api.scryfall.com/cards/random", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const response = await data.json();
+    console.log(props);
+    setCard(response);
+    navigate("/result");
+    console.log(response);
+  }
+
   return (
     <div className="App">
       Search
       <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={fetchTest}>fetch</button>
     </div>
   );
 }
 
-export default App;
+export default Search;
